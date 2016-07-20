23d66b8224d8d52f59c6e004c0fa1cc6612318d6
Gordon <kazrah@gmail.com>
2016-07-20 16:02:32 -0700

    committed existing files into new project

--- Simple1/ViewController.swift
+++ Simple1/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redBtn: UIImageView!
+    
+    @IBOutlet weak var blueBtn: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRed(sender: AnyObject) {
+        
+        redBtn.hidden = true
+        
+    }
+    
+    @IBAction func hideBlue(sender: AnyObject) {
+        
+        blueBtn.hidden = true
+    }
 
 }
 
